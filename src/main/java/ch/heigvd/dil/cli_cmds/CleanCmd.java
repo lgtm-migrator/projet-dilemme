8f66c20f7a7ccf825a5a937ae8fcf853149fb62a
Webblitchy <eliott.chytil@gmail.com>
2022-03-30 17:26:54 +0200

    Add clean command to picocli to remove built sites

--- src/main/java/ch/heigvd/dil/cli_cmds/CleanCmd.java
+++ src/main/java/ch/heigvd/dil/cli_cmds/CleanCmd.java
@@ -3,12 +3,35 @@ package ch.heigvd.dil.cli_cmds;
 import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
+import java.io.*;
 
 @CommandLine.Command(name = "clean", description = "")
 public class CleanCmd implements Callable<Integer> {
   @Override
   public Integer call() {
-    System.out.println("CleanCmd.call has been called !");
+    System.out.println("Cleaning site...");
+
+
+    File file = new File(path + "/build");
+
+    if (!(file.exists() && file.isDirectory())){
+      System.err.println("Error: The build directory does not exist in '"
+              + path + "'");
+      return 1;
+    }
+
+    boolean status = file.delete();
+    if (!status) {
+      System.err.println("Error: Could not delete the build directory");
+      return 1;
+    }
+
+    System.out.println("Done.");
     return 0;
   }
+
+
+  // paramètre indiquant le site à nettoyer
+  @CommandLine.Parameters(description = "The site path to clean")
+  String path;
 }
