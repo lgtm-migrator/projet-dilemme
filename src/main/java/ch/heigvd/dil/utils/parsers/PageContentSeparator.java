013e24e2964ece05d117aa708bebcb681221da81
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-04-06 15:57:55 +0200

    Reformat file following conventions

--- src/main/java/ch/heigvd/dil/utils/parsers/PageContentSeparator.java
+++ src/main/java/ch/heigvd/dil/utils/parsers/PageContentSeparator.java
@@ -1,25 +1,26 @@
 package ch.heigvd.dil.utils.parsers;
+
 import java.text.ParseException;
 
 public class PageContentSeparator {
-    private final static String SEP = "---";
-    private final String content;
-    private final String config;
-    public PageContentSeparator(String file) throws ParseException{
-        int index = file.indexOf(SEP);
-        if (index == -1) {
-            throw new ParseException("Invalid page format", 0);
-        }
-        config = file.substring(0, index);
-        content = file.substring(index + SEP.length());
-    }
+  private static final String SEP = "---";
+  private final String content;
+  private final String config;
 
-    public String getContent () {
-        return content;
+  public PageContentSeparator(String file) throws ParseException {
+    int index = file.indexOf(SEP);
+    if (index == -1) {
+      throw new ParseException("Invalid page format", 0);
     }
+    config = file.substring(0, index);
+    content = file.substring(index + SEP.length());
+  }
 
-    public String getConfig () {
-        return config;
-    }
+  public String getContent() {
+    return content;
+  }
 
+  public String getConfig() {
+    return config;
+  }
 }
