7c5b12e29772e3be38ed87210d79653495ccfcac
superjeffcplusplus <72446750+superjeffcplusplus@users.noreply.github.com>
2022-04-06 23:09:56 +0200

    add and implements PageContentSeparator with simple test (#31)
    * add and implements PageContentSeparator with simple test
    
    * Reformat file following conventions

---
+++ src/main/java/ch/heigvd/dil/utils/parsers/PageContentSeparator.java
@@ -0,0 +1,26 @@
+package ch.heigvd.dil.utils.parsers;
+
+import java.text.ParseException;
+
+public class PageContentSeparator {
+  private static final String SEP = "---";
+  private final String content;
+  private final String config;
+
+  public PageContentSeparator(String file) throws ParseException {
+    int index = file.indexOf(SEP);
+    if (index == -1) {
+      throw new ParseException("Invalid page format", 0);
+    }
+    config = file.substring(0, index);
+    content = file.substring(index + SEP.length());
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public String getConfig() {
+    return config;
+  }
+}
