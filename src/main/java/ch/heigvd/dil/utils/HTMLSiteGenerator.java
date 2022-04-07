b8a7bd6096ffd5a0ac8a91d0f4d5f924a75e3b3c
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-04-07 17:39:22 +0200

    Add HTML Site Generator

---
+++ src/main/java/ch/heigvd/dil/utils/HTMLSiteGenerator.java
@@ -0,0 +1,22 @@
+package ch.heigvd.dil.utils;
+
+import ch.heigvd.dil.data_structures.Site;
+
+public class HTMLSiteGenerator {
+  private final Site site;
+
+  /**
+   * @param site Site pour lequel on veut générer le code HTML.
+   */
+  public HTMLSiteGenerator(Site site) {
+    this.site = site;
+  }
+
+  /**
+   * Generates the HTML code for the site.
+   * @param path Destination path for the generated HTML files.
+   */
+  public void generate(String path) {
+    // TODO: Implement this method
+  }
+}
