dc6ad8a5f9d3eb53ca4e940e54edca22a41f81fa
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-04-19 11:03:06 +0200

    Add config validator to ref config validation

--- src/main/java/ch/heigvd/dil/utils/parsers/PageContentSeparator.java
+++ src/main/java/ch/heigvd/dil/utils/parsers/PageContentSeparator.java
@@ -1,65 +1,56 @@
 package ch.heigvd.dil.utils.parsers;
 
 import ch.heigvd.dil.data_structures.Page;
-import org.everit.json.schema.Schema;
-import org.everit.json.schema.ValidationException;
-import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Permet de lire un fichier représentant une page et de parser
+ * la partie contenu et la partie configuration.
+ */
 public class PageContentSeparator {
   private static final String SEP = "---";
   private final String content;
-  private final String config;
+  private final Page.Config config;
 
+  /**
+   * Le constructeur sépare la partie contenu de la partie configuration
+   * et se charge de vérifier la conformité de la configuration.
+   * @param file String contenant la page non parsée
+   * @throws ParseException si la page ne contient pas de séparateur
+   */
   public PageContentSeparator(String file) throws ParseException {
     int index = file.indexOf(SEP);
     if (index == -1) {
       throw new ParseException("Invalid page format", 0);
     }
-    config = file.substring(0, index);
+    config = genPageConfig(file.substring(0, index));
     content = file.substring(index + SEP.length());
   }
 
+  /**
+   * Récupère la partie contenu de la page
+   * @return contenu de la page
+   */
   public String getContent() {
     return content;
   }
 
+  /**
+   * Récupère la partie configuration de la page
+   * @return configuration valide
+   */
   public Page.Config getConfig() {
-    return genPageConfig(config);
-  }
-
-  private static JSONObject readSchema() {
-    JSONObject out;
-    try (FileInputStream fis = new FileInputStream("schema/page-config-schema.json")) {
-      out = new JSONObject(new JSONTokener(fis));
-    } catch (IOException e) {
-      System.out.println(e.getMessage());
-      throw new RuntimeException();
-    }
-    return out;
-  }
-
-  private static JSONObject validateConfig(String strConfig, JSONObject jsonSchema) {
-    Schema schema = SchemaLoader.load(jsonSchema);
-    JSONObject jsonSubject = new JSONObject(new JSONTokener(strConfig));
-    try {
-      schema.validate(jsonSubject);
-    } catch (ValidationException e) {
-      System.out.println(e.getMessage());
-      throw new RuntimeException("Error in page config");
-    }
-    return jsonSubject;
+    return config;
   }
 
   private static Page.Config genPageConfig(String configStr) {
-    JSONObject conf = validateConfig(configStr, readSchema());
+
+    ConfigValidator validator = new ConfigValidator(configStr, "schema/page-config-schema.json");
+    JSONObject conf = validator.getValidConfig();
     String dateStr = conf.getString("date");
 
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
---
+++ src/main/java/ch/heigvd/dil/utils/parsers/SiteConfigParser.java
@@ -0,0 +1,9 @@
+package ch.heigvd.dil.utils.parsers;
+
+import org.json.JSONObject;
+
+public class SiteConfigParser {
+    SiteConfigParser(String configFile) {
+    }
+
+}
