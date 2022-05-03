3c0a4ec4617e72784b02eedd5ab4a99d2f6a14dd
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-05-03 17:42:27 +0200

    Add a test for MarkdownParser

---
+++ src/test/java/ch/heigvd/dil/utils/parsers/MarkdownParserTest.java
@@ -0,0 +1,30 @@
+package ch.heigvd.dil.utils.parsers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MarkdownParserTest {
+  @Test
+  public void testConvertMarkdownToHtml() {
+    String md =
+        "# Heading\n"
+            + "## Sub-heading\n"
+            + "### Sub-sub-heading\n"
+            + "*italic*\n"
+            + "**bold**\n"
+            + "*[link](http://www.google.com)*\n"
+            + "*[link with title](http://www.google.com \"Google\")*\n";
+    String html = MarkdownParser.convertMarkdownToHTML(md);
+    System.out.println(html);
+    assertEquals(
+        html,
+        "<h1>Heading</h1>\n"
+            + "<h2>Sub-heading</h2>\n"
+            + "<h3>Sub-sub-heading</h3>\n"
+            + "<p><em>italic</em>\n"
+            + "<strong>bold</strong>\n"
+            + "<em><a href=\"http://www.google.com\">link</a></em>\n"
+            + "<em><a href=\"http://www.google.com\" title=\"Google\">link with title</a></em></p>\n");
+  }
+}
