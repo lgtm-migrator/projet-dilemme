7c8d1d9217f5ede39c2c9aac96232caec9e25cf1
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-05-11 15:37:05 +0200

    Implement the `serve` command

--- src/main/java/ch/heigvd/dil/cli_cmds/ServeCmd.java
+++ src/main/java/ch/heigvd/dil/cli_cmds/ServeCmd.java
@@ -1,13 +1,34 @@
 package ch.heigvd.dil.cli_cmds;
 
+import java.io.File;
 import java.util.concurrent.Callable;
+
+import ch.heigvd.dil.Main;
 import picocli.CommandLine;
+import java.awt.Desktop;
+import java.net.URI;
 
 @CommandLine.Command(name = "serve", description = "")
 public class ServeCmd implements Callable<Integer> {
+  @CommandLine.Parameters(description = "The site path to serve")
+  String path;
+
   @Override
   public Integer call() {
-    System.out.println("ServeCmd.call has been called !");
+    int exitCode = new CommandLine(new Main()).execute("build", path);
+    if (exitCode != 0) {
+      return exitCode;
+    }
+
+    if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+      try {
+        Desktop.getDesktop()
+            .browse(new URI("file://" + new File(path).getAbsolutePath() + "/build/index.html"));
+      } catch (Exception e) {
+        System.err.println("Could not open the browser");
+        return 1;
+      }
+    }
     return 0;
   }
 }
