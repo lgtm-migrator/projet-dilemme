2a2f1d1bfc63769469afb7b36e0a290bcf6a3a23
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-05-31 10:36:44 +0200

    add comment

--- src/main/java/ch/heigvd/dil/Main.java
+++ src/main/java/ch/heigvd/dil/Main.java
@@ -18,6 +18,7 @@ import picocli.CommandLine;
 public class Main {
   public static void main(String[] args) {
     int exitCode = new CommandLine(new Main()).execute(args);
+    // Vérification nécessaire pour que la commande serve fonctionne
     if (exitCode != 0) {
       System.exit(exitCode);
     }
