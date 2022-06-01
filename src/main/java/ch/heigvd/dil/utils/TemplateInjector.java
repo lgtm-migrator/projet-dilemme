54b167e7987ac4af4a88297094f6b844b30944f0
Luhrel <57545107+Luhrel@users.noreply.github.com>
2022-06-01 20:58:40 +0200

    Include content of HTML template files

--- src/main/java/ch/heigvd/dil/utils/TemplateInjector.java
+++ src/main/java/ch/heigvd/dil/utils/TemplateInjector.java
@@ -11,16 +11,20 @@ import com.github.jknack.handlebars.context.FieldValueResolver;
 import com.github.jknack.handlebars.context.JavaBeanValueResolver;
 import com.github.jknack.handlebars.context.MapValueResolver;
 import com.github.jknack.handlebars.context.MethodValueResolver;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+
 import java.io.*;
 import java.nio.file.Files;
 
 public class TemplateInjector {
-  private Handlebars handlebars;
-  private Site.Config siteConfig;
+  private final Handlebars handlebars;
+  private final Site site;
 
-  public TemplateInjector(Site.Config siteConfig) {
-    handlebars = new Handlebars();
-    this.siteConfig = siteConfig;
+  public TemplateInjector(Site site) {
+    TemplateLoader loader = new ClassPathTemplateLoader(site.getPath() + "/template", ".html");
+    handlebars = new Handlebars(loader);
+    this.site = site;
   }
 
   public String resolveProperties(File layout, Page page) throws IOException {
@@ -50,14 +54,13 @@ public class TemplateInjector {
 
   public String mergeJSON(Page page) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
-    // TODO: Resolve {% include <filename>.html } elements
 
     ObjectNode rootNode = mapper.createObjectNode();
 
     ObjectNode page_node = mapper.readValue(page.getPageConfig().getJSON(), ObjectNode.class);
     rootNode.set("page", page_node);
 
-    ObjectNode site_node = mapper.readValue(siteConfig.getJSON(), ObjectNode.class);
+    ObjectNode site_node = mapper.readValue(site.getConfig().getJSON(), ObjectNode.class);
     rootNode.set("site", site_node);
 
     String html = MarkdownParser.convertMarkdownToHTML(page.getMarkdown());
